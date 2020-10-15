96a207b6b2e22a58cd2d536ac5add6e1e12d205e
Luizahf <50409713+Luizahf@users.noreply.github.com>
2020-10-15 18:59:01 -0300

    Data binding with obtects

---
+++ DataBinding/app/src/main/java/com/anushka/bindingdemo1/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.anushka.bindingdemo1
+
+import android.annotation.SuppressLint
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.databinding.DataBindingUtil
+import com.anushka.bindingdemo1.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.student = getStudent()
+    }
+
+    private fun getStudent(): Student {
+        return Student(1, "Luiza", "luizahfreire@hotmail.com")
+    }
+}
