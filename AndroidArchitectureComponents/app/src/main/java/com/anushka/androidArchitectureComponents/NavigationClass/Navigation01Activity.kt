2b973811e569ebbc5fe2e3d17dc6f0ba6dc93548
Luizahf <50409713+Luizahf@users.noreply.github.com>
2020-10-20 18:30:47 -0300

    Navigation host fragment

---
+++ AndroidArchitectureComponents/app/src/main/java/com/anushka/androidArchitectureComponents/NavigationClass/Navigation01Activity.kt
@@ -0,0 +1,12 @@
+package com.anushka.androidArchitectureComponents.NavigationClass
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.anushka.androidArchitectureComponents.R
+
+class Navigation01Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_navigation01)
+    }
+}
